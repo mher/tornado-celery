22203c8e18fc8917518f4e22b516f17883b2e032
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-04-16 17:15:38 +0400

    tcelery is a celery command

--- tcelery/__main__.py
+++ tcelery/__main__.py
@@ -2,6 +2,10 @@ from __future__ import absolute_import
 
 import logging
 
+from pprint import pformat
+
+from celery.bin.base import Command
+
 from tornado import ioloop
 from tornado import httpserver
 from tornado.options import options, define, parse_command_line
@@ -13,15 +17,37 @@ from . import setup_nonblocking_producer
 define("port", default=8888, type=bool, help="run on the given port")
 
 
-def main():
-    parse_command_line()
+class TCeleryCommand(Command):
 
-    logging.info("Starting http server on port %s..." % options.port)
-    http_server = httpserver.HTTPServer(Application())
-    http_server.listen(options.port)
-    setup_nonblocking_producer()
-    try:
+    def run_from_argv(self, prog_name, argv=None):
+        argv = filter(self.tornado_option, argv)
+        parse_command_line([prog_name] + argv)
+
+        logging.info("Starting http server on port %s..." % options.port)
+        http_server = httpserver.HTTPServer(Application(celery_app=self.app))
+        http_server.listen(options.port)
+        logging.info("Registered tasks:")
+        logging.info(pformat(self.app.tasks.keys()))
+
+        logging.info("Setting up non-blocking producer...")
+        setup_nonblocking_producer()
+
         ioloop.IOLoop.instance().start()
+
+    def handle_argv(self, prog_name, argv=None):
+        return self.run_from_argv(prog_name, argv)
+
+    @staticmethod
+    def tornado_option(arg):
+        name, _, value = arg.lstrip('-').partition("=")
+        name = name.replace('-', '_')
+        return hasattr(options, name)
+
+
+def main():
+    try:
+        cmd = TCeleryCommand()
+        cmd.execute_from_commandline()
     except KeyboardInterrupt:
         pass
 
--- tcelery/app.py
+++ tcelery/app.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import celery
+
 from tornado import web
 from tornado.options import define, options
 
@@ -11,7 +13,8 @@ define("debug", type=bool, default=False, help="run in debug mode")
 
 
 class Application(web.Application):
-    def __init__(self):
+    def __init__(self, celery_app=None):
         handlers = route.get_routes()
         settings = dict(debug=options.debug)
         super(Application, self).__init__(handlers, **settings)
+        self.celery_app = celery_app or celery.Celery()
