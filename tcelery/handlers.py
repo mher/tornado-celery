531db59af90738df3df8c18512c1d930fa1d7ce6
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-04-16 15:46:35 +0400

    Webserver uses non-blocking api

--- tcelery/handlers.py
+++ tcelery/handlers.py
@@ -6,53 +6,43 @@ from functools import partial
 
 from tornado import web
 from tornado import ioloop
-from tornado.options import options as tornado_options
 from tornado.escape import json_decode
 
 from celery.result import AsyncResult
 from celery.task.control import inspect
 from celery.task.control import revoke
-from celery.execute import send_task
+from celery.utils import uuid
 
 from .utils import route
 
 
 class ApplyHandlerBase(web.RequestHandler):
-    registered_tasks = []
 
     def get_task_args(self):
+        "extracts task args from a request"
         options = json_decode(self.request.body)
         args = options.pop('args', [])
         kwargs = options.pop('kwargs', {})
         return args, kwargs, options
 
-    @classmethod
-    def get_registered_tasks(cls):
-        if not cls.registered_tasks:
-            i = inspect()
-            tasks = []
-            for rt in i.registered().itervalues():
-                tasks.extend(rt)
-            cls.registered_tasks = set(tasks)
-
-        return cls.registered_tasks
-    
 
 @route('/async-apply/(.*)/')
 class AsyncApplyHandler(ApplyHandlerBase):
     def post(self, taskname):
-        if taskname not in self.get_registered_tasks():
-            raise web.HTTPError(404)
+        try:
+            task = self.application.celery_app.tasks[taskname]
+        except KeyError:
+            raise web.HTTPError(404, "Unknown task '%s'" % taskname)
 
         args, kwargs, options = self.get_task_args()
-        result = send_task(taskname, args=args, kwargs=kwargs)
+        result = task.apply_async(args=args, kwargs=kwargs, **options)
         self.write({'task-id': result.task_id, 'state': result.state})
 
 
 @route('/tasks/result/(.*)/')
 class TaskResultHandler(web.RequestHandler):
     def get(self, task_id):
-        result = AsyncResult(task_id)
+        result = AsyncResult(task_id, app=self.application.celery_app)
         response = {'task-id': task_id, 'state': result.state}
         if result.ready():
             response.update({'result': result.result}) 
@@ -68,44 +58,42 @@ class TaskRevokeHandler(web.RequestHandler):
 
 @route('/apply/(.*)/')
 class ApplyHandler(ApplyHandlerBase):
-    tasks = {}
 
     @web.asynchronous
     def post(self, taskname):
-        if not tornado_options.blocking:
-            raise web.HTTPError(503)
-        if taskname not in self.get_registered_tasks():
-            raise web.HTTPError(404)
+        try:
+            task = self.application.celery_app.tasks[taskname]
+        except KeyError:
+            raise web.HTTPError(404, "Unknown task '%s'" % taskname)
 
         args, kwargs, options = self.get_task_args()
         timeout = options.pop('timeout', None)
-        result = send_task(taskname, args=args, kwargs=kwargs, **options)
+        task_id = uuid()
 
         htimeout = None
         if timeout:
             htimeout = ioloop.IOLoop.instance().add_timeout(
                     timedelta(seconds=timeout),
-                    partial(ApplyHandler.on_time, result.task_id))
+                    partial(self.on_time, task_id))
 
-        self.tasks[result.task_id] = (result, self, htimeout)
+        task.apply_async(args=args, kwargs=kwargs, task_id=task_id,
+                         callback=partial(self.on_complete, htimeout),
+                         **options)
 
-    @classmethod
-    def on_complete(cls, task_id):
-        result, handler, htimeout = cls.tasks.pop(task_id)
-        response = {'task-id': task_id, 'state': result.state}
+    def on_complete(self, htimeout, result):
+        response = {'task-id': result.task_id, 'state': result.state}
         if result.successful():
             response.update({'result': result.result})
-        handler.write(response)
+        self.write(response)
         if htimeout:
             ioloop.IOLoop.instance().remove_timeout(htimeout)
-        handler.finish()
+        self.finish()
     
-    @classmethod
-    def on_time(cls, task_id):
-        result, handler, _ = cls.tasks.pop(task_id)
+    def on_time(self, task_id):
         revoke(task_id)
-        handler.write({'task-id': task_id, 'state': result.state})
-        handler.finish()
+        result = AsyncResult(task_id, app=self.application.celery_app)
+        self.write({'task-id': task_id, 'state': result.state})
+        self.finish()
 
 
 @route('/tasks/registered/(.*)')
