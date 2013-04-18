af5737ddc2d4e572a4f2587206ae1d2151f8df7c
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-04-18 19:15:07 +0400

    Updates benchmarks

--- tests/benchmarks.py
+++ tests/benchmarks.py
@@ -3,54 +3,33 @@ from __future__ import absolute_import
 import sys
 import time
 
-from functools import partial
-from urllib import urlencode
-
-import requests
-
 from tornado import ioloop
-from tornado import httpclient
 
-
-echo_url = "http://localhost:8888/apply/tasks.echo/"
-async_echo_url = "http://localhost:8888/apply-async/tasks.echo/"
-
-
-def _timeit_fetch(url, ntimes):
-    time_start = time.time()
-    def done(response, n):
-        if n==ntimes-1:
-            ioloop.IOLoop.instance().stop()
-
-    http_client = httpclient.AsyncHTTPClient()
-    for i in range(ntimes):
-        http_client.fetch(url, partial(done, n=i), method="POST", 
-                          body=urlencode({"args": i}))
-    ioloop.IOLoop.instance().start()
-    return ntimes, time.time() - time_start
-
-
-def bench_apply(ntimes):
-    print("apply %d times: %ss" % _timeit_fetch(echo_url, ntimes))
+import tasks
 
 
 def bench_apply_async(ntimes):
-    print("apply-async %d times: %ss" % _timeit_fetch(async_echo_url, ntimes))
-
-
-def bench_apply_async_with_requests(ntimes):
     time_start = time.time()
     for i in range(ntimes):
-        requests.post(async_echo_url, data={"args":"foo"})
-    print("apply-async-with-requests %d times: %ss" % (ntimes, time.time() - time_start))
+        tasks.add.apply_async(args=[i, i])
+    print("apply_async called {} times in {} seconds".format(
+            ntimes, time.time() - time_start))
 
 
-def bench_delay(ntimes):
-    from examples.tasks import echo
-    time_start = time.time()
-    for i in range(ntimes):
-        echo.delay(i)
-    print("delay %d times: %ss" % (ntimes, time.time() - time_start))
+def bench_apply_async_nonblocking(ntimes, stop_io_loop=False):
+    io_loop = ioloop.IOLoop.instance()
+    def publish():
+        time_start = time.time()
+        for i in range(ntimes):
+            tasks.add.apply_async(args=[i, i])
+        print("non blocking apply_async called {} times in {} seconds".format(
+                ntimes, time.time() - time_start))
+        if stop_io_loop:
+            io_loop.stop()
+
+    import tcelery
+    tcelery.setup_nonblocking_producer(io_loop=io_loop, on_ready=publish)
+    io_loop.start()
 
 
 if __name__=="__main__":
@@ -59,7 +38,5 @@ if __name__=="__main__":
     if method:
         vars()[method](ntimes)
     else:
-        bench_delay(ntimes)
-        bench_apply_async_with_requests(ntimes)
         bench_apply_async(ntimes)
-        bench_apply(ntimes)
+        bench_apply_async_nonblocking(ntimes)
