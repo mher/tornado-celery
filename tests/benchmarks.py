765be3c2f41b65f21d5bf734362f34d487b8003c
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-04-22 23:31:17 +0400

    Benchmarks don't share celery apps

--- tests/benchmarks.py
+++ tests/benchmarks.py
@@ -5,13 +5,27 @@ import time
 
 from tornado import ioloop
 
-import tasks
+from celery import Celery
+
+
+celery1 = Celery('tasks', broker='amqp://')
+celery2 = Celery('tasks', broker='amqp://')
+
+
+@celery1.task
+def add(x, y):
+    return x + y
+
+
+@celery2.task
+def echo(x):
+    return x
 
 
 def bench_apply_async(ntimes):
     time_start = time.time()
     for i in range(ntimes):
-        tasks.add.apply_async(args=[i, i])
+        add.apply_async(args=[i, i])
     print("apply_async called {} times in {} seconds".format(
         ntimes, time.time() - time_start))
 
@@ -22,22 +36,27 @@ def bench_apply_async_nonblocking(ntimes, stop_io_loop=False):
     def publish():
         time_start = time.time()
         for i in range(ntimes):
-            tasks.add.apply_async(args=[i, i])
+            echo.apply_async(args=[i])
         print("non blocking apply_async called {} times in {} seconds".format(
                 ntimes, time.time() - time_start))
         if stop_io_loop:
             io_loop.stop()
 
     import tcelery
-    tcelery.setup_nonblocking_producer(io_loop=io_loop, on_ready=publish)
+    tcelery.setup_nonblocking_producer(celery_app=celery2,
+                                       io_loop=io_loop,
+                                       on_ready=publish)
     io_loop.start()
 
 
 if __name__ == "__main__":
     ntimes = int(sys.argv[1]) if len(sys.argv) > 1 else 100
     method = sys.argv[2] if len(sys.argv) == 3 else None
-    if method:
-        vars()[method](ntimes)
-    else:
-        bench_apply_async(ntimes)
-        bench_apply_async_nonblocking(ntimes)
+    try:
+        if method:
+            vars()[method](ntimes)
+        else:
+            bench_apply_async(ntimes)
+            bench_apply_async_nonblocking(ntimes)
+    except KeyboardInterrupt:
+        pass
