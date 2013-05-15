12f3a308d9f1cfae50cdfcc21e8092d37afc2375
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-05-16 02:52:00 +0400

    Revert "pika accepts x_expires in milliseconds"
    This reverts commit 52d2827965db567baacceaabdfaca2c3b300bd3a.

--- tcelery/producer.py
+++ tcelery/producer.py
@@ -65,9 +65,9 @@ class NonBlockingTaskProducer(TaskProducer):
 
     def prepare_expires(self, value=None, type=None):
         if value is None:
-            value = self.app.conf.CELERY_TASK_RESULT_EXPIRES * 1000
+            value = self.app.conf.CELERY_TASK_RESULT_EXPIRES
         if isinstance(value, timedelta):
-            value = timeutils.timedelta_seconds(value)
+            value = timeutils.timedelta_seconds(value) * 1000
         if value is not None and type:
             return type(value)
         return value
