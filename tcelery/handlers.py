792cd3608222478e54ba051ce847fadd592d4b7f
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-04-20 17:54:04 +0400

    Document api

--- tcelery/handlers.py
+++ tcelery/handlers.py
@@ -16,18 +16,64 @@ from .utils import route
 
 
 class ApplyHandlerBase(web.RequestHandler):
-
     def get_task_args(self):
         "extracts task args from a request"
-        options = json_decode(self.request.body)
+        try:
+            options = json_decode(self.request.body)
+        except (TypeError, ValueError):
+            raise web.HTTPError(400)
         args = options.pop('args', [])
         kwargs = options.pop('kwargs', {})
+        if not isinstance(args, (list, tuple)):
+            raise web.HTTPError(400, 'task args must be a list or tuple')
+
         return args, kwargs, options
 
 
 @route('/apply-async/(.*)/')
-class AsyncApplyHandler(ApplyHandlerBase):
+class ApplyAsyncHandler(ApplyHandlerBase):
     def post(self, taskname):
+        """
+Apply tasks asynchronously by sending a message
+
+**Example request**:
+
+.. sourcecode:: http
+
+    POST /apply-async/examples.tasks.add/ HTTP/1.1
+    Accept: application/json
+    Accept-Encoding: gzip, deflate, compress
+    Content-Length: 16
+    Content-Type: application/json; charset=utf-8
+    Host: localhost:8888
+    User-Agent: HTTPie/0.8.0
+
+    {
+        "args": [
+            1,
+            2
+        ]
+    }
+
+
+**Example response**:
+
+.. sourcecode:: http
+
+    HTTP/1.1 200 OK
+    Content-Length: 71
+    Content-Type: application/json; charset=UTF-8
+    Server: TornadoServer/3.2
+
+    {
+        "state": "PENDING",
+        "task-id": "1c9be31f-3094-4319-8895-ad2f0654c699"
+    }
+
+:statuscode 200: no error
+:statuscode 400: invalid request
+:statuscode 404: unknown task
+        """
         try:
             task = self.application.celery_app.tasks[taskname]
         except KeyError:
@@ -37,14 +83,46 @@ class AsyncApplyHandler(ApplyHandlerBase):
         result = task.apply_async(args=args, kwargs=kwargs, **options)
         self.write({'task-id': result.task_id, 'state': result.state})
 
-
 @route('/tasks/result/(.*)/')
 class TaskResultHandler(web.RequestHandler):
     def get(self, task_id):
+        """
+Get task result by task-id
+
+**Example request**:
+
+.. sourcecode:: http
+
+    GET /tasks/result/9ec42ba0-be59-488f-a445-4a007d83b954/ HTTP/1.1
+    Accept: application/json
+    Accept-Encoding: gzip, deflate, compress
+    Content-Type: application/json; charset=utf-8
+    Host: localhost:8888
+    User-Agent: HTTPie/0.8.0
+
+**Example response**:
+
+.. sourcecode:: http
+
+    HTTP/1.1 200 OK
+    Content-Length: 84
+    Content-Type: application/json; charset=UTF-8
+    Etag: "0aef8448588cf040f1daa7a0244c0a7b93abfd71"
+    Server: TornadoServer/3.2
+
+    {
+        "result": 3,
+        "state": "SUCCESS",
+        "task-id": "9ec42ba0-be59-488f-a445-4a007d83b954"
+    }
+
+:statuscode 200: no error
+:statuscode 400: invalid request
+        """
         result = AsyncResult(task_id, app=self.application.celery_app)
         response = {'task-id': task_id, 'state': result.state}
         if result.ready():
-            if response.successful():
+            if result.successful():
                 response['result'] = result.result
             else:
                 response['traceback'] = result.traceback
@@ -55,15 +133,86 @@ class TaskResultHandler(web.RequestHandler):
 @route('/tasks/revoke/(.*)/')
 class TaskRevokeHandler(web.RequestHandler):
     def delete(self, task_id):
+        """
+Revoke a task
+
+**Example request**:
+
+.. sourcecode:: http
+
+    DELETE /tasks/revoke/d776e835-33ac-447f-b27d-bb8529718ae6/ HTTP/1.1
+    Accept: application/json
+    Accept-Encoding: gzip, deflate, compress
+    Content-Length: 0
+    Content-Type: application/json; charset=utf-8
+    Host: localhost:8888
+    User-Agent: HTTPie/0.8.0
+
+**Example response**:
+
+.. sourcecode:: http
+
+    HTTP/1.1 200 OK
+    Content-Length: 51
+    Content-Type: application/json; charset=UTF-8
+    Server: TornadoServer/3.2
+
+    {
+        "task-id": "d776e835-33ac-447f-b27d-bb8529718ae6"
+    }
+
+:statuscode 200: no error
+:statuscode 400: invalid request
+        """
         revoke(task_id)
         self.write({'task-id': task_id})
 
 
 @route('/apply/(.*)/')
 class ApplyHandler(ApplyHandlerBase):
-
     @web.asynchronous
     def post(self, taskname):
+        """
+Apply tasks synchronously. Function returns when the task is finished
+
+**Example request**:
+
+.. sourcecode:: http
+
+    POST /apply/examples.tasks.add/ HTTP/1.1
+    Accept: application/json
+    Accept-Encoding: gzip, deflate, compress
+    Content-Length: 16
+    Content-Type: application/json; charset=utf-8
+    Host: localhost:8888
+    User-Agent: HTTPie/0.8.0
+
+    {
+        "args": [
+            1,
+            2
+        ]
+    }
+
+**Example response**:
+
+.. sourcecode:: http
+
+    HTTP/1.1 200 OK
+    Content-Length: 84
+    Content-Type: application/json; charset=UTF-8
+    Server: TornadoServer/3.2
+
+    {
+        "result": 3,
+        "state": "SUCCESS",
+        "task-id": "2ce70595-a028-4e0d-b906-be2183fc6821"
+    }
+
+:statuscode 200: no error
+:statuscode 400: invalid request
+:statuscode 404: unknown task
+        """
         try:
             task = self.application.celery_app.tasks[taskname]
         except KeyError:
--- tcelery/utils.py
+++ tcelery/utils.py
@@ -1,18 +1,13 @@
-import tornado.web
-
-
 class route(object):
     """route decorator from https://github.com/peterbe/tornado-utils"""
     _routes = []
 
-    def __init__(self, uri, name=None):
-        self._uri = uri
-        self._name = name
+    def __init__(self, regexp):
+        self._regexp = regexp
 
     def __call__(self, handler):
         """gets called when we class decorate"""
-        name = self._name or handler.__name__
-        self._routes.append(tornado.web.url(self._uri, handler, name=name))
+        self._routes.append((self._regexp, handler))
         return handler
 
     @classmethod
