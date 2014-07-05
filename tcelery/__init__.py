e66aa6ac5cdca9096a8fcb854f1713e605e8592e
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-07-05 11:10:38 +0400

    Bump up development version to 0.4

--- tcelery/__init__.py
+++ tcelery/__init__.py
@@ -8,7 +8,7 @@ from .connection import ConnectionPool
 from .producer import NonBlockingTaskProducer
 from .result import AsyncResult
 
-VERSION = (0, 3, 4)
+VERSION = (0, 4, 0)
 __version__ = '.'.join(map(str, VERSION)) + '-dev'
 
 
