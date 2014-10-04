eb5477e12d0d762d0bdbbe730f6077b10f3dea9f
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-10-04 20:29:44 +0400

    Cleanup

--- examples/tasks.py
+++ examples/tasks.py
@@ -4,28 +4,28 @@ from datetime import datetime
 
 from celery import Celery
 
-celery = Celery("tasks", broker="amqp://guest:guest@localhost:5672")
+
+celery = Celery("tasks", broker="amqp://")
 celery.conf.CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'amqp')
 
 
-@celery.task(name='tasks.add')
+@celery.task
 def add(x, y):
     return int(x) + int(y)
 
 
-@celery.task(name='tasks.sleep')
+@celery.task
 def sleep(seconds):
     time.sleep(float(seconds))
     return seconds
 
 
-@celery.task(name='tasks.echo')
+@celery.task
 def echo(msg, timestamp=False):
-    print "echooooo"
     return "%s: %s" % (datetime.now(), msg) if timestamp else msg
 
 
-@celery.task(name='tasks.error')
+@celery.task
 def error(msg):
     raise Exception(msg)
 
