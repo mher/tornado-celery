85b625dcf78a8a1d7e9ad79fa81eadbb69ca4858
Simon Hewitt <si@sjhewitt.co.uk>
2014-10-07 12:03:06 -0700

    allow producer class to be overridden in setup_nonblocking_producer

--- tcelery/__init__.py
+++ tcelery/__init__.py
@@ -14,21 +14,21 @@ __version__ = '.'.join(map(str, VERSION)) + '-dev'
 
 def setup_nonblocking_producer(celery_app=None, io_loop=None,
                                on_ready=None, result_cls=AsyncResult,
-                               limit=1):
+                               limit=1, producer_cls=NonBlockingTaskProducer):
     celery_app = celery_app or celery.current_app
     io_loop = io_loop or ioloop.IOLoop.instance()
 
-    NonBlockingTaskProducer.app = celery_app
-    NonBlockingTaskProducer.conn_pool = ConnectionPool(limit, io_loop)
-    NonBlockingTaskProducer.result_cls = result_cls
+    producer_cls.app = celery_app
+    producer_cls.conn_pool = ConnectionPool(limit, io_loop)
+    producer_cls.result_cls = result_cls
     if celery_app.conf['BROKER_URL'] and celery_app.conf['BROKER_URL'].startswith('amqp'):
-        celery.app.amqp.AMQP.producer_cls = NonBlockingTaskProducer
+        celery.app.amqp.AMQP.producer_cls = producer_cls
 
     def connect():
         broker_url = celery_app.connection().as_uri(include_password=True)
         options = celery_app.conf.get('CELERYT_PIKA_OPTIONS', {})
-        NonBlockingTaskProducer.conn_pool.connect(broker_url,
-                                                  options=options,
-                                                  callback=on_ready)
+        producer_cls.conn_pool.connect(broker_url,
+                                       options=options,
+                                       callback=on_ready)
 
     io_loop.add_callback(connect)
